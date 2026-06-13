6363a149f0b6ed5947f460bf271dd7be0060c38c
yy <dinglenutsxnex@gmail.com>
2026-06-13 12:16:54 +0530

    Last Sync: 2026-06-13 12:16 (Mobile)

--- app/src/main/java/com/mitm/shadowtrack/AppState.kt
+++
@@ -1,7 +0,0 @@
-package com.mitm.shadowtrack
-
-import com.mitm.shadowtrack.model.ConnectionViewModel
-
-object AppState {
-    val viewModel: ConnectionViewModel by lazy { ConnectionViewModel() }
-}
